81422d787fdb029057b91d767792a3dec64d6fac
Tomas Jansson <tomas.jansson@gmail.com>
2024-05-24 00:25:23 +0200

    Main slide layout

--- src/app/slides/_slides/slide-layouts.tsx
+++ src/app/slides/_slides/slide-layouts.tsx
@@ -1,3 +1,4 @@
+import Image, { StaticImageData } from "next/image";
 import { PropsWithChildren } from "react";
 
 export const TwoColumns = ({ children }: PropsWithChildren) => {
@@ -8,10 +9,36 @@ export const ThreeColumns = ({ children }: PropsWithChildren) => {
   return <div className="grid grid-cols-3 gap-4">{children}</div>;
 };
 
-export const MainSlide = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="h-screen w-screen p-4 px-12 text-5xl flex justify-center items-center text-center leading-loose">
-      {children}
+const BackgroundImageWithBackdrop = ({
+  imgData,
+}: {
+  imgData: StaticImageData;
+}) => (
+  <div className="absolute top-0 left-0 w-screen h-screen">
+    <Image
+      src={imgData}
+      alt="background"
+      className="w-full h-full object-cover"
+    />
+    <div className="absolute top-0 left-0 w-screen h-screen bg-black/80">
+      &nbsp;
     </div>
+  </div>
+);
+
+export const MainSlide = ({
+  children,
+  imgUrl,
+}: PropsWithChildren<{
+  imgUrl?: StaticImageData;
+}>) => {
+  console.log("==> imageUrl: ", imgUrl);
+  return (
+    <>
+      {imgUrl && <BackgroundImageWithBackdrop imgData={imgUrl} />}
+      <div className="absolute h-screen w-screen p-4 px-12 text-5xl flex justify-center items-center text-center leading-loose bg-transparent z-10">
+        {children}
+      </div>
+    </>
   );
 };
