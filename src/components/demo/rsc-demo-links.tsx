47424950a4dbdeb05c545ae7a8e3aa28038be297
Tomas Jansson <tomas.jansson@gmail.com>
2024-06-01 00:05:55 +0200

    Minor corrections

--- src/components/demo/rsc-demo-links.tsx
+++ src/components/demo/rsc-demo-links.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 export const RscDemoLinks = (props: Props) => {
   return (
-    <div className="flex flex-row gap-2 mt-8">
+    <div className="flex flex-row gap-2 mt-8 w-full justify-center">
       <Link onClick={(e) => e.stopPropagation()} href="/demos/rsc1">
-        Only server
+        Only servers
       </Link>
       <Link onClick={(e) => e.stopPropagation()} href="/demos/rsc2">
         Client components
@@ -15,7 +15,7 @@ export const RscDemoLinks = (props: Props) => {
       <Link onClick={(e) => e.stopPropagation()} href="/demos/rsc3">
         Nested components
       </Link>
-      <Link onClick={(e) => e.stopPropagation()} href="/slides/13">
+      <Link onClick={(e) => e.stopPropagation()} href="/slides/14">
         Slides
       </Link>
     </div>
