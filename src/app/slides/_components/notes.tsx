611d0d1479a4caa03cdb884b939e459e254265b2
Tomas Jansson <tomas.jansson@gmail.com>
2024-06-07 08:38:09 +0200

    Minor styling changes

--- src/app/slides/_components/notes.tsx
+++ src/app/slides/_components/notes.tsx
@@ -2,6 +2,8 @@ import { PropsWithChildren } from "react";
 
 export const Notes = ({ children }: PropsWithChildren) => {
   return (
-    <div className="hidden group-[.show-notes]:block notes">{children}</div>
+    <div className="hidden group-[.show-notes]:block notes max-h-screen overflow-auto">
+      {children}
+    </div>
   );
 };
